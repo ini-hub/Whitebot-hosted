c730e588dbe5c6cd207889017f7bf363859d42e4
Emmanuel Adelakun <devtenotea@gmail.com>
2023-06-24 02:18:00 +0100

    chore: complete mission vision section

--- src/assets/images/images.index.ts
+++ src/assets/images/images.index.ts
@@ -5,9 +5,12 @@ export { default as IM_HeroVideoThumbnail } from "./im_hero-video-thumbnail.svg"
 export { default as IM_HeroLogoIllustrationLeft } from "./im_hero-logo-illutration-left.svg";
 export { default as IM_HeroLogoIllustrationRight } from "./im_hero-logo-illutration-right.svg";
 export { default as IM_BackgroundSkin } from "./im_background-skin.svg";
+export { default as IM_BackgroundSkinTwo } from "./im_background-skin-two.svg";
 export { default as IM_AffordabilityIllustration } from "./im_affordability-illustration.svg";
 export { default as IM_ImmobilityIllustration } from "./im_immobility-illustration.svg";
 export { default as IM_InsecurityIllustration } from "./im_insecurity-illustration.svg";
 export { default as IM_StigmatizationIllustration } from "./im_stigmatization-illustration.svg";
 export { default as IM_GoalsDesktopIllustration } from "./im_goals-desktop-illustration.svg";
 export { default as IM_GoalsMobileIllustration } from "./im_goals-mobile-illustration.svg";
+export { default as IM_Binoculars } from "./im_binoculars.svg";
+export { default as IM_MissionTarget } from "./im_mission-target.svg";
