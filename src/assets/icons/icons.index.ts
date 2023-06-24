1f5a28888af8f8f28c677a8e02ae359092973c6c
Emmanuel Adelakun <devtenotea@gmail.com>
2023-06-24 05:23:32 +0100

    feat: complete footer section

--- src/assets/icons/icons.index.ts
+++ src/assets/icons/icons.index.ts
@@ -11,4 +11,9 @@ export { default as IC_Community } from "./ic_commnuity.svg";
 export { default as IC_Handshake } from "./ic_handshake.svg";
 export { default as IC_Hamburger } from "./ic_hamburger.svg";
 export { default as IC_Play } from "./ic_play.svg";
+export { default as IC_Facebook } from "./ic_facebook.png";
+export { default as IC_Instagram } from "./ic_instagram.png";
+export { default as IC_Twitter } from "./ic_twitter.png";
+export { default as IC_Youtube } from "./ic_youTube.png";
+export { default as IC_ArrowUpCircle } from "./ic_arrow-up-circle.svg";
 // export { default as } from './'
---
+++ src/chunks/index-template/FooterChunk.tsx
@@ -0,0 +1,145 @@
+import {
+  IC_ArrowUpCircle,
+  IC_Facebook,
+  IC_Instagram,
+  IC_Twitter,
+  IC_Youtube,
+} from "../../assets/icons/icons.index";
+import { IM_CompanyLogo } from "../../assets/images/images.index";
+import BaseButton from "../../components/buttons/BaseButton";
+
+function useFooterChunk() {
+  const socialMediaLinks = [
+    { id: 1, icon: IC_Instagram, link: "https://instagram.com" },
+    { id: 2, icon: IC_Twitter, link: "https://twitter.com" },
+    { id: 3, icon: IC_Youtube, link: "https://youtube.com" },
+    { id: 4, icon: IC_Facebook, link: "https://facebook.com" },
+  ];
+
+  return { socialMediaLinks };
+}
+
+export default function FooterChunk() {
+  const { socialMediaLinks } = useFooterChunk();
+  return (
+    <footer className="bg-white">
+      <div className="max-w-screen-xl mx-auto w-11/12 pt-14 pb-6">
+        <div className="grid grid-cols-2 lg:grid-cols-5 mb-8 gap-y-10">
+          <div>
+            <h5 className="font-semibold text-lg">Company</h5>
+            <div className="grid gap-3 mt-3">
+              <a href="" className="text-sm text-gray-500 block max-w-max">
+                Our Story
+              </a>
+              <a href="" className="text-sm text-gray-500 block max-w-max">
+                FAQ
+              </a>
+              <a href="" className="text-sm text-gray-500 block max-w-max">
+                Careers
+              </a>
+            </div>
+          </div>
+          <div>
+            <h5 className="font-semibold text-lg">Products</h5>
+            <div className="grid gap-3 mt-3">
+              <a href="" className="text-sm text-gray-500 block max-w-max">
+                White Bot
+              </a>
+              <a href="" className="text-sm text-gray-500 block max-w-max">
+                CamBot
+              </a>
+            </div>
+          </div>
+          <div>
+            <h5 className="font-semibold text-lg">Contact Us</h5>
+            <div className="grid gap-3 mt-3">
+              <a
+                href="tel:+2348138303956"
+                className="text-sm text-gray-500 block max-w-max"
+              >
+                0813 830 3956
+              </a>
+
+              <a
+                href="mailto:info.walkit@gmail.com"
+                className="text-sm text-gray-500 block max-w-max"
+              >
+                info.walkit@gmail.com
+              </a>
+            </div>
+          </div>
+          <div className="lg:hidden">
+            <span> Follow us</span>
+            <div className="flex items-center gap-2">
+              {socialMediaLinks.map((link) => (
+                <a href={link.link} key={link.id}>
+                  <img src={link.icon} className="h-[30px]" />
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div>
+              <div className="flex items-center justify-between">
+                <div>
+                  <h5 className="font-semibold text-lg">Got Questions?</h5>
+                  <p className="text-xs text-gray-500">We have an answer</p>
+                </div>
+                <button
+                  className="block lg:hidden"
+                  onClick={() => window.scrollTo(0, 0)}
+                >
+                  <img src={IC_ArrowUpCircle} className="h-[40px]" />
+                </button>
+              </div>
+              <div className="grid gap-3 mt-5">
+                <input
+                  type="email"
+                  required
+                  placeholder="Email Address"
+                  className="bg-[#F3F3F3] rounded-md text-sm py-3 px-5"
+                />
+                <textarea
+                  required
+                  placeholder="Enter your message"
+                  className="bg-[#F3F3F3] rounded-md text-sm py-3 px-5 resize-none h-[80px]"
+                />
+                <button className="bg-[#FAF4FF] py-3 rounded-md text-center block text-sm font-medium text-app-purple">
+                  Send
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          className="hidden lg:block mx-auto"
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          <img src={IC_ArrowUpCircle} className="h-[40px]" />
+        </button>
+        <div className="mt-4">
+          <div className="flex items-start justify-between border-b py-5">
+            <img src={IM_CompanyLogo} />
+            <div className="hidden lg:block">
+              <span> Follow us</span>
+              <div className="flex items-center gap-2">
+                {socialMediaLinks.map((link) => (
+                  <a href={link.link} key={link.id}>
+                    <img src={link.icon} className="h-[30px]" />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center flex-wrap gap-y-4 justify-between mt-3">
+            <p>
+              <a className="text-xs">Privacy Policy</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+              <a className="text-xs">Terms of Services</a>
+            </p>
+            <p className="text-xs">© 2023 Walk IT, all rights reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
